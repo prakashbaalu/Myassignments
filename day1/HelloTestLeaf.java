3f63da278d355682a68a0a819b4d431a8adc198a
prakashbaalu <95171204+prakashbaalu@users.noreply.github.com>
2025-04-26 22:59:50 +0530

    Add files via upload

---
+++ day1/HelloTestLeaf.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class HelloTestLeaf {
+	public static void main(String[] args) {
+		
+		byte age=32;
+				
+		System.out.println(age);
+		System.out.println("Welcome test leaf");
+		
+		
+				
+		
+	}
+
+}
