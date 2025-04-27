a1e8d8fab6acf6a137a8477a02e7a22f5e387db7
prakashbaalu <95171204+prakashbaalu@users.noreply.github.com>
2025-04-27 14:10:49 +0530

    Add files via upload

--- day1/HelloTestLeaf.java
+++ day1/HelloTestLeaf.java
@@ -4,9 +4,14 @@ public class HelloTestLeaf {
 	public static void main(String[] args) {
 		
 		byte age=32;
-				
-		System.out.println(age);
-		System.out.println("Welcome test leaf");
+		long phoneNumber=7397326605L;
+		short year=1989;
+		boolean elgibileForAutomation=true;
+		
+		System.out.println("Prakash is:" + age);
+		System.out.println("Prakash PhoneNo:" + phoneNumber);
+		System.out.println("Prakash born in :" + year);
+		System.out.println("Prakash Join in Test leaf");
 		
 		
 				
